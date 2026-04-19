602812125df951d70fb35aa716e182361e505510
TrabucooX <juanbelmontegonzalez@gmail.com>
2026-04-19 21:01:09 +0200

    Performing soft hyperparameter tuning and programming main.py file.

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+
+def evaluate_model():
+    df = pd.read_csv("../data/Consumer_Shopping_Trends_2026.csv")
+
+    model = joblib.load("../data/final_xgb_model.pkl")
+    encoder = joblib.load("../data/label_encoder.pkl")
+
+    # We prepare data to evaluate
+    df_encoded = encoder.transform(df)
+    X = df_encoded.drop("shopping_preference", axis=1)
+    y = df_encoded["shopping_preference"]
+
+    _, X_test, _, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+    predictions = model.predict(X_test)
+
+    # Classification report
+    print("Global Performance")
+    print(classification_report(y_test, predictions, target_names=["Hybrid", "Online", "Store"]))
+
+    # Confusion matrix
+    print("Confusion matrix across the three labels")
+    print(confusion_matrix(y_test, predictions, labels=["Hybrid", "Online", "Store"]))
+
+    # Displaying confusion matrix for better visualization
+    
+
+
+if __name__ == "__main__":
+    evaluate_model()
